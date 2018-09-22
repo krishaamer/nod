181943e0521f330a79f1d348ed4807a38612c48d
Kris Haamer <kris@haam.co>
2018-09-23 00:58:15 +0200

    Check for sending vs receiving flow

--- nod-client/client/startup/startup.js
+++ nod-client/client/startup/startup.js
@@ -1,9 +1,9 @@
 Meteor.startup(() => {
 
-	Session.set('invite_activity', {});
-	Session.set('invite_when', {});
-	Session.set('invite_location', {});
-	Session.set('invite_friends', {});
-	Session.set('invite_friends_phone', {});
+	Session.set('invite_activity', false);
+	Session.set('invite_when', false);
+	Session.set('invite_location', false);
+	Session.set('invite_friends', false);
+	Session.set('invite_friends_phone', false);
 	Session.set("invite_share_sms_done", false);
 });
