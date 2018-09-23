bf13679075949d8aaaa8c513d62a083d83a10d9f
Kris Haamer <kris@haam.co>
2018-09-23 12:24:36 +0200

    Try to fix SEO

--- nod-client/client/router/router.js
+++ nod-client/client/router/router.js
@@ -1,5 +1,65 @@
 import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 
+FlowRouter.globals.push({
+  meta: {
+    'charset': {
+      charset: 'UTF-8',
+    },
+    'keywords': {
+      name: 'keywords',
+      itemprop: 'keywords',
+      content: 'social, friends, app, activities',
+    },
+    'description': {
+      name: 'description',
+      itemprop: 'description',
+      property: 'og:description',
+      content: Meteor.settings["public"].SITE_DESC,
+    },
+    'image': {
+      name: 'twitter:image',
+      itemprop: 'image',
+      property: 'og:image',
+      content: Meteor.settings["public"].DEFAULT_SHARE_IMG,
+    },
+    'google': {
+      name: 'google-site-verification',
+      content: Meteor.settings["public"].GOOGLE_SITE_VERIFICATION,
+    },
+    'og:type': {
+      'property': 'og:type',
+      'content': 'website',
+    },
+    'og:title': {
+      'property': 'og:title',
+      'content' () {
+        return document.title;
+      },
+    },
+    'og:site_name': {
+      'property': 'og:site_name',
+      'content': Meteor.settings["public"].SITE_NAME,
+    },
+    'url': {
+      property: 'og:url',
+      itemprop: 'url',
+      content () {
+        return window.location.href;
+      }
+    },
+    'twitter:card': 'summary',
+    'twitter:title' () {
+      return document.title;
+    },
+    'twitter:description': Meteor.settings["public"].SITE_DESC,
+    'http-equiv': {
+      'http-equiv': 'X-UA-Compatible',
+      'content': 'IE=edge,chrome=1'
+    },
+    'robots': 'index, follow',
+  },
+});
+
 FlowRouter.route('/', {
   name: 'activity', 
   title: 'New activity', 
